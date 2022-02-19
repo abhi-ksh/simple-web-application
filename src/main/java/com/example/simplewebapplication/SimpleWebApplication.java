5620bad41d240959a8773528d3576f73dc0b43d0
Abhijit Kshirsagar <kshiabhi@amazon.com>
2022-02-18 23:29:51 -0500

    initial commit

---
+++ src/main/java/com/example/simplewebapplication/SimpleWebApplication.java
@@ -0,0 +1,29 @@
+package com.example.simplewebapplication;
+
+import ch.qos.logback.core.CoreConstants;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class SimpleWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleWebApplication.class, args);
+    }
+    @Bean
+    public CommandLineRunner CommandLineRunner(ApplicationContext ctx){
+        return args -> {
+            System.out.println("Let's inspect the beans!");
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for(String beanName : beanNames){
+                System.out.println(beanName);
+            }
+        };
+    }
+}
